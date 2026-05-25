a635abb2260a36c700e2ea429e9a06c37e8ed394
Tharuneswar <dtharuneswar@aditya.ac.in>
2026-05-25 06:23:36 +0000

    Solved Distance Between Bus Stops

---
+++ distance-between-bus-stops.py
@@ -0,0 +1,12 @@
+def distanceBetweenBusStops(distance, start, destination):
+    # Calculate the total distance of the circular route
+    total_distance = sum(distance)
+    
+    # Calculate the distance from start to destination in the clockwise direction
+    clockwise_distance = sum(distance[min(start, destination):max(start, destination)])
+    
+    # Calculate the distance from start to destination in the counter-clockwise direction
+    counter_clockwise_distance = total_distance - clockwise_distance
+    
+    # Return the minimum distance between the two directions
+    return min(clockwise_distance, counter_clockwise_distance)
