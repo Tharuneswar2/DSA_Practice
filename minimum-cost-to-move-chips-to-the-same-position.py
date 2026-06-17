d663f5152c488f95a5906e00d041434db5e4834f
Tharuneswar <dtharuneswar@aditya.ac.in>
2026-06-17 05:04:41 +0000

    Solved Minimum Cost to Move Chips to The Same Position

---
+++ minimum-cost-to-move-chips-to-the-same-position.py
@@ -0,0 +1,17 @@
+def minCostToMoveChips(position):
+    # Initialize variables to store the count of chips at odd and even positions
+    odd_count = 0
+    even_count = 0
+    
+    # Iterate over each position
+    for pos in position:
+        # If the position is odd, increment the odd count
+        if pos % 2 != 0:
+            odd_count += 1
+        # If the position is even, increment the even count
+        else:
+            even_count += 1
+    
+    # The minimum cost is the minimum of the count of chips at odd positions and the count of chips at even positions
+    # This is because we can move all chips to either an odd or an even position with a cost equal to the count of chips at the other type of position
+    return min(odd_count, even_count)
