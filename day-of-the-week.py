8e2e822b68e38dabcad0cd9a67ce121422d823ab
Tharuneswar <dtharuneswar@aditya.ac.in>
2026-06-20 04:23:26 +0000

    Solved Day of the Week

---
+++ day-of-the-week.py
@@ -0,0 +1,7 @@
+def dayOfTheWeek(day, month, year):
+    t = [0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4]
+    year -= month < 3
+    return ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"][(year + int(year/4) - int(year/100) + int(year/400) + t[month-1] + day) % 7]
+
+# Test the function
+print(dayOfTheWeek(31, 8, 2019))  # Output: Saturday
