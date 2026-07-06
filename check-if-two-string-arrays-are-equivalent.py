863a55419eea0f4b84bfa49f786e51735f76a472
Tharuneswar <dtharuneswar@aditya.ac.in>
2026-07-06 04:20:17 +0000

    Solved Check If Two String Arrays are Equivalent

---
+++ check-if-two-string-arrays-are-equivalent.py
@@ -0,0 +1,10 @@
+def arrayStringsAreEqual(word1, word2):
+    # Join all strings in word1 into a single string
+    str1 = ''.join(word1)
+    
+    # Join all strings in word2 into a single string
+    str2 = ''.join(word2)
+    
+    # Compare the two strings
+    # If they are equal, return True; otherwise, return False
+    return str1 == str2
