94996b1555aeface69321a203419ffd5eae05565
Tharuneswar <dtharuneswar@aditya.ac.in>
2026-07-17 03:47:55 +0000

    Solved Lexicographically Smallest Palindrome (approach 2)

--- lexicographically-smallest-palindrome.py
+++ lexicographically-smallest-palindrome.py
@@ -1,31 +1,44 @@
+# Solution approach 2 - provide an efficient python solution with detailed inline comments explaining each step
 def makeSmallestPalindrome(num):
-    # Convert the number to a string
+    # Convert the number to a string to easily access and manipulate digits
     num_str = str(num)
     
-    # Find the middle index of the string
-    mid = len(num_str) // 2
+    # Initialize two pointers, one at the start and one at the end of the string
+    left = 0
+    right = len(num_str) - 1
     
-    # If the string length is odd, the middle character can be anything
-    if len(num_str) % 2 != 0:
-        # Make the first half of the string the smallest possible
-        first_half = '0' * mid
-        # The middle character can be '0' to make the palindrome smallest
-        middle = '0'
-        # The second half is the reverse of the first half
-        second_half = first_half[::-1]
-        # Combine the three parts to form the smallest palindrome
-        return int(first_half + middle + second_half)
-    else:
-        # If the string length is even, the middle two characters must be the same
-        # Make the first half of the string the smallest possible
-        first_half = '0' * (mid - 1)
-        # The last character of the first half must be '1' to make the palindrome smallest
-        first_half += '1'
-        # The second half is the reverse of the first half
-        second_half = first_half[::-1]
-        # Combine the two parts to form the smallest palindrome
-        return int(first_half + second_half)
-
-# Test the function
-print(makeSmallestPalindrome(100))  # Output: 101
-print(makeSmallestPalindrome(1234))  # Output: 1001
+    # Continue the loop until the two pointers meet
+    while left < right:
+        # If the left digit is greater than the right digit, increment the right digit
+        if num_str[left] > num_str[right]:
+            # If the right digit is '9', we need to carry the increment to the next digit
+            if num_str[right] == '9':
+                # Initialize a carry variable to track the carry
+                carry = 1
+                # Initialize a pointer to the current right digit
+                i = right
+                # Continue the loop until we find a digit that is not '9' or we reach the start of the string
+                while i >= 0 and num_str[i] == '9':
+                    # Set the current digit to '0'
+                    num_str = num_str[:i] + '0' + num_str[i+1:]
+                    # Decrement the pointer
+                    i -= 1
+                    # If we reached the start of the string, we need to add a new digit at the start
+                    if i < 0:
+                        num_str = '1' + num_str
+                        # Break the loop
+                        break
+                # If we didn't reach the start of the string, increment the digit that is not '9'
+                else:
+                    num_str = num_str[:i] + str(int(num_str[i]) + carry) + num_str[i+1:]
+            # If the right digit is not '9', simply increment it
+            else:
+                num_str = num_str[:right] + str(int(num_str[right]) + 1) + num_str[right+1:]
+            # Move the right pointer to the left
+            right -= 1
+        # If the left digit is not greater than the right digit, move the left pointer to the right
+        else:
+            left += 1
+    
+    # Convert the string back to an integer and return it
+    return int(num_str)
