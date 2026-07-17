292e729dde027855c24e954a85ed5ffb473f451b
Tharuneswar <dtharuneswar@aditya.ac.in>
2026-07-17 08:38:18 +0000

    Solved Maximum Difference Between Increasing Elements (approach 2)

--- maximum-difference-between-increasing-elements.py
+++ maximum-difference-between-increasing-elements.py
@@ -1,20 +1,17 @@
+# Solution approach 2 - provide an efficient python solution with detailed inline comments explaining each step
 def maximumDifference(nums):
     # Initialize minimum value and maximum difference
-    min_val = nums[0]
-    max_diff = 0
+    min_val = nums[0]  # assume the first element as the minimum value
+    max_diff = 0  # initialize maximum difference as 0
     
     # Iterate through the list of numbers
     for num in nums:
-        # If current number is smaller than min_val, update min_val
+        # If current number is smaller than the minimum value, update the minimum value
         if num < min_val:
-            min_val = num
-        # If difference between current number and min_val is greater than max_diff, update max_diff
+            min_val = num  # update the minimum value
+        # If the difference between the current number and the minimum value is greater than the maximum difference, update the maximum difference
         elif num - min_val > max_diff:
-            max_diff = num - min_val
+            max_diff = num - min_val  # update the maximum difference
     
     # Return the maximum difference
     return max_diff
-
-# Test the function
-print(maximumDifference([7,1,5,4]))  # Output: 4
-print(maximumDifference([9,4,3,2]))  # Output: 0
