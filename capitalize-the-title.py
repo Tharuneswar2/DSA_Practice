3f7cb053f5285ace68457e3bf93b87ebb914efad
Tharuneswar <dtharuneswar@aditya.ac.in>
2026-07-17 03:48:04 +0000

    Solved Capitalize the Title (approach 2)

--- capitalize-the-title.py
+++ capitalize-the-title.py
@@ -1,3 +1,4 @@
+# Solution approach 2 - provide an efficient python solution with detailed inline comments explaining each step
 def capitalizeTitle(title: str) -> str:
     # Split the title into words
     words = title.split()
@@ -7,14 +8,17 @@ def capitalizeTitle(title: str) -> str:
     
     # Iterate over each word in the title
     for word in words:
-        # If the word has more than 2 characters, capitalize it
+        # If the word has more than 2 characters, capitalize the first letter and make the rest lowercase
         if len(word) > 2:
-            capitalized_words.append(word.capitalize())
-        # If the word has 2 or less characters, convert it to lowercase
+            capitalized_word = word.capitalize()
+        # If the word has 2 or less characters, make it lowercase
         else:
-            capitalized_words.append(word.lower())
+            capitalized_word = word.lower()
+        
+        # Add the capitalized word to the list
+        capitalized_words.append(capitalized_word)
     
-    # Join the capitalized words back into a string
+    # Join the capitalized words back into a string with spaces in between
     capitalized_title = ' '.join(capitalized_words)
     
     # Return the capitalized title
