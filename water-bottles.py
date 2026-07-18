e02922a135325756f0c04d0dc57be1b8265adce8
Tharuneswar <dtharuneswar@aditya.ac.in>
2026-07-18 08:27:33 +0000

    Solved Water Bottles (approach 2)

--- water-bottles.py
+++ water-bottles.py
@@ -1,19 +1,21 @@
+# Solution approach 2 - provide an efficient python solution with detailed inline comments explaining each step
 def numWaterBottles(numBottles, numExchange):
     # Initialize the total number of bottles that can be drunk
     total_drunk = numBottles
     
-    # Initialize the number of empty bottles
-    empty_bottles = numBottles
+    # Initialize the remaining empty bottles
+    remaining_empty = numBottles
     
-    # Continue the process until we cannot exchange empty bottles for a new one
-    while empty_bottles >= numExchange:
-        # Calculate the number of new bottles we can get by exchanging empty bottles
-        new_bottles = empty_bottles // numExchange
+    # Continue the process until there are not enough empty bottles to exchange
+    while remaining_empty >= numExchange:
+        # Calculate the number of new bottles that can be obtained by exchanging the empty bottles
+        new_bottles = remaining_empty // numExchange
         
         # Update the total number of bottles that can be drunk
         total_drunk += new_bottles
         
-        # Update the number of empty bottles
-        empty_bottles = new_bottles + (empty_bottles % numExchange)
+        # Update the remaining empty bottles
+        remaining_empty = new_bottles + (remaining_empty % numExchange)
     
+    # Return the total number of bottles that can be drunk
     return total_drunk
