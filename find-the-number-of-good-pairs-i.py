e9b1358bca736d8d7028c4343efe965e3a66c827
Tharuneswar <dtharuneswar@aditya.ac.in>
2026-07-19 08:55:50 +0000

    Solved Find the Number of Good Pairs I (approach 2)

--- find-the-number-of-good-pairs-i.py
+++ find-the-number-of-good-pairs-i.py
@@ -1,18 +1,20 @@
+# Solution approach 2 - provide an efficient python solution with detailed inline comments explaining each step
 def numIdenticalPairs(nums):
-    # Create a hashmap to store the frequency of each number
+    # Create a hashmap to store the frequency of each number in the array
     freq_map = {}
     
-    # Initialize the count of good pairs
+    # Initialize the count of good pairs to 0
     good_pairs = 0
     
-    # Iterate over the list of numbers
+    # Iterate over each number in the array
     for num in nums:
-        # If the number is already in the hashmap, it means we have found a pair
+        # If the number is already in the hashmap, it means we have seen it before
         if num in freq_map:
-            # The number of pairs that can be formed with this number is equal to its frequency
+            # The number of good pairs that can be formed with this number is equal to its frequency
+            # This is because each occurrence of the number can form a pair with every other occurrence
             good_pairs += freq_map[num]
         
-        # Increment the frequency of the current number
+        # Increment the frequency of the current number in the hashmap
         freq_map[num] = freq_map.get(num, 0) + 1
     
     # Return the total count of good pairs
