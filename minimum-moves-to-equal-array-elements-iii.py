ba2c6539880325e70452560f8a00dbdf68d6539f
Tharuneswar <dtharuneswar@aditya.ac.in>
2026-07-20 04:13:34 +0000

    Solved Minimum Moves to Equal Array Elements III (approach 2)

--- minimum-moves-to-equal-array-elements-iii.py
+++ minimum-moves-to-equal-array-elements-iii.py
@@ -1,17 +1,18 @@
+# Solution approach 2 - provide an efficient python solution with detailed inline comments explaining each step
 def minMoves2(nums):
-    # First, sort the array
+    # First, we sort the array in ascending order
     nums.sort()
     
-    # Calculate the median of the array
+    # We find the median of the array, which will be the target value that all elements will be equal to
     median = nums[len(nums) // 2]
     
-    # Initialize the total moves
+    # Initialize a variable to store the total number of moves
     total_moves = 0
     
-    # For each number in the array, calculate the absolute difference with the median
-    # This difference represents the minimum number of moves required to make the number equal to the median
+    # Iterate over each number in the array
     for num in nums:
+        # For each number, the number of moves required to make it equal to the median is the absolute difference between the number and the median
         total_moves += abs(num - median)
     
-    # Return the total moves
+    # Return the total number of moves
     return total_moves
