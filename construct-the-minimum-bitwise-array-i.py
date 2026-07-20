1c2ea956e6fc19b989ee42a02f6c4faf5440e21a
Tharuneswar <dtharuneswar@aditya.ac.in>
2026-07-20 04:19:42 +0000

    Solved Construct the Minimum Bitwise Array I (approach 2)

--- construct-the-minimum-bitwise-array-i.py
+++ construct-the-minimum-bitwise-array-i.py
@@ -1,15 +1,26 @@
-def min_bitwise_array(nums):
-    # Initialize the result array with the first element of the input array
-    res = [nums[0]]
-    
-    # Iterate over the input array starting from the second element
-    for i in range(1, len(nums)):
-        # Calculate the bitwise AND of the current element and the last element in the result array
-        # This is because the bitwise AND operation has the property that a & (a & b) = a & b
-        # So, we can keep track of the bitwise AND of all elements seen so far by maintaining the last element in the result array
-        res.append(nums[i] & res[-1])
-    
-    return res
+# Solution approach 2 - provide an efficient python solution with detailed inline comments explaining each step
 
-# Test the function
-print(min_bitwise_array([1, 2, 3, 4, 5]))
+def minBitwiseORArray(nums):
+    # Initialize an empty list to store the result
+    res = []
+    
+    # Iterate over the input list
+    for num in nums:
+        # If the result list is empty or the current number is less than the last number in the result list, 
+        # append the current number to the result list
+        if not res or num < res[-1]:
+            res.append(num)
+        else:
+            # Initialize a variable to store the bitwise OR of the current number and the last number in the result list
+            bitwise_or = res[-1] | num
+            
+            # If the bitwise OR is equal to the last number in the result list, 
+            # it means the current number does not change the result, so we skip it
+            if bitwise_or == res[-1]:
+                continue
+            else:
+                # Otherwise, append the bitwise OR to the result list
+                res.append(bitwise_or)
+    
+    # Return the result list
+    return res
