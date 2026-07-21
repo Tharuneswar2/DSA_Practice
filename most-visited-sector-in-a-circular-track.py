95f29a6ed6c3b4be64e3a9a24adc0d2f50efef65
Tharuneswar <dtharuneswar@aditya.ac.in>
2026-07-21 09:18:34 +0000

    Solved Most Visited Sector in  a Circular Track (approach 2)

--- most-visited-sector-in-a-circular-track.py
+++ most-visited-sector-in-a-circular-track.py
@@ -1,16 +1,19 @@
-def mostVisited(n, rounds):
-    # Initialize variables to store the start and end of the range
-    start, end = rounds[0], rounds[-1]
-    
-    # If the end is less than the start, it means we have to consider the circular nature of the track
-    if end < start:
-        # In this case, we need to consider two ranges: from the start to the end of the track, and from the beginning of the track to the end
-        return list(range(start, n + 1)) + list(range(1, end + 1))
-    else:
-        # If the end is not less than the start, we can simply return the range from the start to the end
-        return list(range(start, end + 1))
+# Solution approach 2 - provide an efficient python solution with detailed inline comments explaining each step
 
-# Test the function
-print(mostVisited(3, [1, 2]))  # Output: [1, 2]
-print(mostVisited(4, [1, 3]))  # Output: [1, 2, 3, 4]
-print(mostVisited(7, [1, 5]))  # Output: [1, 2, 3, 4, 5]
+def mostVisited(self, n: int, rounds: List[List[int]]) -> List[int]:
+    # Calculate the total number of sectors visited
+    total_sectors = rounds[-1][0] - rounds[0][0] + 1
+    
+    # If the total number of sectors visited is equal to the total number of sectors in the track
+    # Then all sectors are visited
+    if total_sectors == n:
+        return list(range(1, n + 1))
+    
+    # If the start sector is less than or equal to the end sector
+    # Then all sectors between the start and end sector are visited
+    if rounds[0][0] <= rounds[-1][0]:
+        return list(range(rounds[0][0], rounds[-1][0] + 1))
+    
+    # If the start sector is greater than the end sector
+    # Then all sectors from the start sector to the end of the track and from the start of the track to the end sector are visited
+    return list(range(rounds[0][0], n + 1)) + list(range(1, rounds[-1][0] + 1))
