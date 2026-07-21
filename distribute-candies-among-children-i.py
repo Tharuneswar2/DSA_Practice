2842656b0ead7bbd50fa9f3638f23ac4da7ee608
Tharuneswar <dtharuneswar@aditya.ac.in>
2026-07-21 03:55:15 +0000

    Solved Distribute Candies Among Children I (approach 2)

--- distribute-candies-among-children-i.py
+++ distribute-candies-among-children-i.py
@@ -1,18 +1,26 @@
+# Solution approach 2 - provide an efficient python solution with detailed inline comments explaining each step
 def distributeCandies(candies, num_people):
-    # Initialize an array to store the number of candies each person gets
-    distribution = [0] * num_people
+    # Initialize the candies given to each person as 0
+    candies_given = [0] * num_people
     
-    # Initialize the index of the current person and the number of candies given
-    index, given = 0, 0
+    # Initialize the index of the current person
+    i = 0
     
-    # Continue distributing candies until all candies are given
-    while given < candies:
-        # Calculate the number of candies the current person gets
-        # This is the minimum of the remaining candies and the current index plus one
-        distribution[index] += min(candies - given, index + 1)
+    # Continue distributing candies until there are no more candies left
+    while candies > 0:
+        # Calculate the number of candies to give to the current person
+        # This is the minimum of the number of candies left and the index of the current person plus 1
+        # This ensures that we don't give more candies than we have left
+        candies_to_give = min(candies, i + 1)
         
-        # Update the number of candies given and the index of the current person
-        given += index + 1
-        index = (index + 1) % num_people
+        # Give the calculated number of candies to the current person
+        candies_given[i] += candies_to_give
+        
+        # Subtract the number of candies given from the total number of candies
+        candies -= candies_to_give
+        
+        # Move to the next person
+        i = (i + 1) % num_people
     
-    return distribution
+    # Return the list of candies given to each person
+    return candies_given
