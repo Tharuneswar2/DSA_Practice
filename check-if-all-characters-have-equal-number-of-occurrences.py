fe23bc7cf429c39d0939fd087471b0fa42928bc1
Tharuneswar <dtharuneswar@aditya.ac.in>
2026-07-21 09:18:47 +0000

    Solved Check if All Characters Have Equal Number of Occurrences (approach 2)

--- check-if-all-characters-have-equal-number-of-occurrences.py
+++ check-if-all-characters-have-equal-number-of-occurrences.py
@@ -1,24 +1,25 @@
-def are_all_chars_equal(s):
-    # Create a dictionary to store the frequency of each character
-    char_freq = {}
+# Solution approach 2 - provide an efficient python solution with detailed inline comments explaining each step
+def areOccurrencesEqual(s: str) -> bool:
+    # Create a dictionary to store the frequency of each character in the string
+    char_frequency = {}
     
-    # Iterate over the string to count the frequency of each character
+    # Iterate over each character in the string
     for char in s:
-        if char in char_freq:
-            char_freq[char] += 1
+        # If the character is already in the dictionary, increment its count
+        if char in char_frequency:
+            char_frequency[char] += 1
+        # If the character is not in the dictionary, add it with a count of 1
         else:
-            char_freq[char] = 1
+            char_frequency[char] = 1
     
-    # Get the frequency of the first character
-    expected_freq = char_freq[s[0]]
+    # Get the frequency of the first character in the string
+    expected_frequency = char_frequency[s[0]]
     
-    # Check if all characters have the same frequency
-    for freq in char_freq.values():
-        if freq != expected_freq:
+    # Iterate over the frequency of each character in the dictionary
+    for frequency in char_frequency.values():
+        # If any character has a different frequency than the expected frequency, return False
+        if frequency != expected_frequency:
             return False
     
+    # If all characters have the same frequency, return True
     return True
-
-# Test the function
-print(are_all_chars_equal("aabbcc"))  # True
-print(are_all_chars_equal("aabbbbcc"))  # False
