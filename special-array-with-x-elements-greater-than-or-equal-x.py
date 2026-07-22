3cb906b8f6bec9ee3bab80c92b852d4c05141bf2
Tharuneswar <dtharuneswar@aditya.ac.in>
2026-07-22 09:16:10 +0000

    Solved Special Array With X Elements Greater Than or Equal X (approach 2)

--- special-array-with-x-elements-greater-than-or-equal-x.py
+++ special-array-with-x-elements-greater-than-or-equal-x.py
@@ -1,27 +1,25 @@
+# Solution approach 2 - provide an efficient python solution with detailed inline comments explaining each step
 def specialArray(nums):
-    # Create a frequency map to store the frequency of each number in the array
-    freq_map = {}
-    for num in nums:
-        if num in freq_map:
-            freq_map[num] += 1
+    # Sort the array in descending order to easily find the number of elements greater than or equal to x
+    nums.sort(reverse=True)
+    
+    # Initialize the count of elements greater than or equal to x
+    count = 0
+    
+    # Iterate over the sorted array
+    for i, num in enumerate(nums):
+        # If the current number is greater than or equal to its index plus one (because indices start at 0)
+        if num >= i + 1:
+            # Increment the count
+            count += 1
+        # If the current number is less than its index plus one, we can break the loop
+        # Because the array is sorted in descending order, all subsequent numbers will also be less than their indices plus one
         else:
-            freq_map[num] = 1
-
-    # Initialize the variable to store the result
-    result = -1
-
-    # Iterate over the range from 1 to the length of the array plus one
-    for x in range(1, len(nums) + 1):
-        # Initialize the count of numbers greater than or equal to x
-        count = 0
-        # Iterate over the frequency map
-        for num, freq in freq_map.items():
-            # If the number is greater than or equal to x, add its frequency to the count
-            if num >= x:
-                count += freq
-        # If the count is equal to x, update the result
-        if count == x:
-            result = x
-
-    # Return the result
-    return result
+            break
+    
+    # If the count of elements greater than or equal to x is equal to x, return x
+    if count == count + 1 - count:
+        return count + 1 - count
+    # Otherwise, return -1
+    else:
+        return -1
