e3ec8864d61a4db8fffb6111f3d36f4d267f6e00
Tharuneswar <dtharuneswar@aditya.ac.in>
2026-07-24 09:08:03 +0000

    Solved Sort Integers by The Number of 1 Bits (approach 2)

--- sort-integers-by-the-number-of-1-bits.py
+++ sort-integers-by-the-number-of-1-bits.py
@@ -1,10 +1,5 @@
+# Solution approach 2 - provide an efficient python solution with detailed inline comments explaining each step
 def sortByBits(arr):
-    # Use a custom sorting key that first considers the number of 1 bits in the binary representation of a number
-    # and then the number itself in case of a tie
+    # Use a custom sorting key that first sorts by the number of 1 bits in the binary representation of each number
+    # and then by the number itself in case of a tie
     return sorted(arr, key=lambda x: (bin(x).count('1'), x))
-
-# Alternatively, you can use a more efficient approach by using the built-in bit_count function (Python 3.10+)
-def sortByBits(arr):
-    # Use a custom sorting key that first considers the number of 1 bits in the binary representation of a number
-    # and then the number itself in case of a tie
-    return sorted(arr, key=lambda x: (x.bit_count(), x))
