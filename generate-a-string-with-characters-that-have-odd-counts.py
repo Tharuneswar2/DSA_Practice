51fd2f8c9bb718332b00f6b91e4a5d7213e553ef
Tharuneswar <dtharuneswar@aditya.ac.in>
2026-07-26 04:08:22 +0000

    Solved Generate a String With Characters That Have Odd Counts (approach 2)

--- generate-a-string-with-characters-that-have-odd-counts.py
+++ generate-a-string-with-characters-that-have-odd-counts.py
@@ -1,9 +1,11 @@
+# Solution approach 2 - provide an efficient python solution with detailed inline comments explaining each step
 def generateTheString(n: int) -> str:
     # If n is odd, we can simply return a string of 'a' repeated n times
     if n % 2 != 0:
+        # This is because 'a' appears an odd number of times in the resulting string
         return 'a' * n
-    
-    # If n is even, we need to return a string with one 'a' and the rest 'b's
-    # This way, 'a' has an odd count (1) and 'b' has an even count (n-1)
     else:
-        return 'a' + 'b' * (n - 1)
+        # If n is even, we need to ensure that the resulting string has characters with odd counts
+        # We can achieve this by appending 'b' to the string of 'a' repeated n-1 times
+        # This way, 'a' appears an odd number of times (n-1) and 'b' appears an odd number of times (1)
+        return 'a' * (n-1) + 'b'
