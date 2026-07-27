a1f49f9520e0ced6ad61eed4df5cc5d1a008d85b
Tharuneswar <dtharuneswar@aditya.ac.in>
2026-07-27 10:32:37 +0000

    Solved Maximum Odd Binary Number (approach 2)

--- maximum-odd-binary-number.py
+++ maximum-odd-binary-number.py
@@ -1,23 +1,22 @@
-def maximum_odd_binary_number(n):
-    # Convert the number to binary and remove the '0b' prefix
+# Solution approach 2 - provide an efficient python solution with detailed inline comments explaining each step
+
+def maximumOddBinaryNumber(n):
+    # Convert the number to binary and remove the '0b' prefix that Python adds
     binary = bin(n)[2:]
     
-    # Find the first '0' from the right
-    zero_index = len(binary) - 1 - binary[::-1].find('0')
+    # Find the first occurrence of '0' from the right
+    # This is because we want to maximize the number of '1's on the left
+    zero_index = binary.rfind('0')
     
-    # If no '0' is found, return the number itself (it's already the maximum odd binary number)
-    if zero_index == len(binary):
+    # If no '0' is found, it means the number is already a maximum odd binary number
+    if zero_index == -1:
+        # So, we simply return the number as it is
         return n
     
-    # Flip the first '0' from the right to '1'
-    binary = binary[:zero_index] + '1' + binary[zero_index + 1:]
+    # If '0' is found, we replace it with '1' to maximize the number of '1's on the left
+    # We also make sure that the number remains odd by appending '1' at the end
+    # We use string slicing to replace the '0' with '1' and append '1' at the end
+    binary = binary[:zero_index] + '1' + binary[zero_index+1:] + '1'
     
-    # Flip all '1's to the right of the flipped '0' to '0's
-    binary = binary[:zero_index + 1] + '0' * (len(binary) - zero_index - 1)
-    
-    # Convert the binary string back to an integer
+    # Finally, we convert the binary string back to an integer and return it
     return int(binary, 2)
-
-# Test the function
-print(maximum_odd_binary_number(10))  # Output: 9
-print(maximum_odd_binary_number(7))   # Output: 7
