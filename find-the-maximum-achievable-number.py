bb5d3f8f04c3d879f41435eacfabe051b7d5edc3
Tharuneswar <dtharuneswar@aditya.ac.in>
2026-07-27 04:17:28 +0000

    Solved Find the Maximum Achievable Number (approach 2)

--- find-the-maximum-achievable-number.py
+++ find-the-maximum-achievable-number.py
@@ -1,28 +1,25 @@
-def maximum_number(num, k):
-    # Convert the number to a list of characters for easier manipulation
+# Solution approach 2 - provide an efficient python solution with detailed inline comments explaining each step
+
+def maximumNumber(num, change):
+    # Convert the number into a list of characters for easier manipulation
     num = list(str(num))
     
-    # Initialize a stack to store the digits
-    stack = []
+    # Iterate over the digits of the number and the change list simultaneously
+    for i, (n, c) in enumerate(zip(num, change)):
+        # If the current digit is less than the corresponding digit in the change list
+        if n < str(c):
+            # Replace the current digit with the digit from the change list
+            num[i] = str(c)
+            # Break the loop as we've found the first digit to replace
+            break
+        # If the current digit is greater than the corresponding digit in the change list
+        elif n > str(c):
+            # Break the loop as we can't make the number larger
+            break
     
-    # Iterate over each digit in the number
-    for digit in num:
-        # While the stack is not empty, the top of the stack is less than the current digit, and we have swaps left
-        while stack and stack[-1] < digit and k:
-            # Remove the top of the stack (swap it out)
-            stack.pop()
-            # Decrement the number of swaps left
-            k -= 1
-        # Add the current digit to the stack
-        stack.append(digit)
-    
-    # If we still have swaps left, remove the last k digits from the stack
-    if k:
-        stack = stack[:-k]
-    
-    # Join the stack into a string and convert it back to an integer
-    return int(''.join(stack))
-
-# Test the function
-print(maximum_number(2736, 1))  # Output: 7236
-print(maximum_number(9973, 2))  # Output: 9973
+    # If we've replaced any digits, join the list back into a string and convert to an integer
+    if num != list(str(num)):
+        return int(''.join(num))
+    else:
+        # If we haven't replaced any digits, return the original number
+        return num
