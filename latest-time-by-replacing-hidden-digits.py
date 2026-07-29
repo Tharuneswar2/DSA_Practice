dd1085f5477febbf643fdf3e65acd09417a73ef7
Tharuneswar <dtharuneswar@aditya.ac.in>
2026-07-29 09:23:50 +0000

    Solved Latest Time by Replacing Hidden Digits (approach 2)

--- latest-time-by-replacing-hidden-digits.py
+++ latest-time-by-replacing-hidden-digits.py
@@ -1,27 +1,27 @@
+# Solution approach 2 - provide an efficient python solution with detailed inline comments explaining each step
 def maximumTime(time: str) -> str:
-    time = list(time)
+    # Convert the input string into a list of characters for easier manipulation
+    time_list = list(time)
     
-    # If the first digit is '?', it can be either 0 or 1 or 2
-    if time[0] == '?':
-        if time[1] == '?' or time[1] > '3':
-            time[0] = '2'
-        else:
-            time[0] = '1' if time[1] == '?' else '2'
+    # Check if the first digit is '2' and the second digit is not '4', if so, replace the second digit with '4'
+    if time_list[0] == '2' and time_list[1] != '4':
+        time_list[1] = '4'
     
-    # If the second digit is '?', it can be either 0-9 if the first digit is 0 or 1, 
-    # otherwise it can be 0-3
-    if time[1] == '?':
-        if time[0] == '2':
-            time[1] = '3'
-        else:
-            time[1] = '9'
+    # Check if the first digit is not '2' and is not '1', if so, replace the first digit with '1'
+    elif time_list[0] != '2' and time_list[0] != '1':
+        time_list[0] = '1'
     
-    # If the third digit is '?', it can be either 0-5
-    if time[3] == '?':
-        time[3] = '5'
+    # Check if the second digit is not '9' and the first digit is '1', if so, replace the second digit with '9'
+    if time_list[0] == '1' and time_list[1] != '9':
+        time_list[1] = '9'
     
-    # If the fourth digit is '?', it can be either 0-9
-    if time[4] == '?':
-        time[4] = '9'
+    # Check if the fourth digit is not '5', if so, replace the fourth digit with '5'
+    if time_list[3] != '5':
+        time_list[3] = '5'
     
-    return ''.join(time)
+    # Check if the fifth digit is not '9', if so, replace the fifth digit with '9'
+    if time_list[4] != '9':
+        time_list[4] = '9'
+    
+    # Join the list of characters back into a string
+    return ''.join(time_list)
