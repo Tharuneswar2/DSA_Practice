050479f957e62027063f94b65c8c547d961d3d38
Tharuneswar <dtharuneswar@aditya.ac.in>
2026-07-29 03:55:12 +0000

    Solved Find the K-th Character in String Game I (approach 2)

--- find-the-k-th-character-in-string-game-i.py
+++ find-the-k-th-character-in-string-game-i.py
@@ -5,10 +5,25 @@ def findKthCharacter(s, k):
     
     # Loop through the string s
     for char in s:
-        # Concatenate the string with itself and the character
-        concatenated_str += char * (len(concatenated_str) + 1)
+        # Concatenate the character to the concatenated string
+        concatenated_str += char
         
-        # If the length of the concatenated string is greater than or equal to k
-        if len(concatenated_str) >= k:
-            # Return the character at index k - 1
-            return concatenated_str[k - 1]
+        # If the length of the concatenated string is equal to k, return the character
+        if len(concatenated_str) == k:
+            return char
+        
+        # If the length of the concatenated string is greater than k, 
+        # it means the k-th character is in the concatenated string
+        if len(concatenated_str) > k:
+            # Return the k-th character in the concatenated string
+            return concatenated_str[k-1]
+            
+    # If the length of the concatenated string is less than k, 
+    # it means the k-th character is not in the concatenated string
+    # So, we need to concatenate the string s to itself
+    while len(concatenated_str) < k:
+        # Concatenate the string s to the concatenated string
+        concatenated_str += s
+        
+    # Return the k-th character in the concatenated string
+    return concatenated_str[k-1]
