e48d44b1b4ad410b14d50d14476e124cc1cd73f6
Tharuneswar <dtharuneswar@aditya.ac.in>
2026-07-30 03:51:25 +0000

    Solved Minimum Cuts to Divide a Circle (approach 2)

--- minimum-cuts-to-divide-a-circle.py
+++ minimum-cuts-to-divide-a-circle.py
@@ -1,32 +1,19 @@
-def min_cuts_to_divide_circle(n):
-    # If the number of cuts is less than 2, we can't divide the circle
+# Solution approach 2 - provide an efficient python solution with detailed inline comments explaining each step
+def numberOfCuts(n: int) -> int:
+    # If the number of cuts is 1 or less, we can't divide the circle into more than 1 piece
     if n < 2:
         return 0
     
-    # Initialize a list to store the minimum cuts for each number of cuts
-    dp = [0] * (n + 1)
+    # If the number of cuts is 2, we can divide the circle into 2 pieces
+    if n == 2:
+        return 1
     
-    # For 2 cuts, we can divide the circle into 2 parts
-    dp[2] = 1
+    # If the number of cuts is even, we can divide the circle into n//2 pieces
+    if n % 2 == 0:
+        # We need n//2 - 1 cuts to divide the circle into n//2 pieces
+        return n // 2
     
-    # For 3 cuts, we can divide the circle into 3 parts
-    dp[3] = 2
-    
-    # For more than 3 cuts, we can divide the circle into more parts
-    for i in range(4, n + 1):
-        # Initialize the minimum cuts for the current number of cuts
-        min_cuts = i - 1
-        
-        # Try all possible previous cuts
-        for j in range(2, i):
-            # Update the minimum cuts if we can divide the circle into more parts
-            min_cuts = min(min_cuts, dp[j] + dp[i - j] + 1)
-        
-        # Store the minimum cuts for the current number of cuts
-        dp[i] = min_cuts
-    
-    # Return the minimum cuts for the given number of cuts
-    return dp[n]
-
-print(min_cuts_to_divide_circle(4))  # Output: 3
-print(min_cuts_to_divide_circle(6))  # Output: 4
+    # If the number of cuts is odd, we can divide the circle into (n+1)//2 pieces
+    else:
+        # We need (n+1)//2 - 1 cuts to divide the circle into (n+1)//2 pieces
+        return (n + 1) // 2
