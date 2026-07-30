5174e6c9eef00cce2b815afc797b964fd5bcfa86
Tharuneswar <dtharuneswar@aditya.ac.in>
2026-07-30 09:16:45 +0000

    Solved Count Odd Numbers in an Interval Range (approach 2)

--- count-odd-numbers-in-an-interval-range.py
+++ count-odd-numbers-in-an-interval-range.py
@@ -1,15 +1,17 @@
+# Solution approach 2 - provide an efficient python solution with detailed inline comments explaining each step
 def countOdds(low: int, high: int) -> int:
-    # Calculate the total number of integers in the range
+    # Calculate the total number of integers in the range [low, high]
     total_numbers = high - low + 1
     
-    # If the total number of integers is odd, then the number of odd integers is half of the total plus one
-    # If the total number of integers is even, then the number of odd integers is half of the total
-    # But we need to consider the case when the low number is odd or even
-    # If the low number is odd, then the number of odd integers is half of the total plus one
-    # If the low number is even, then the number of odd integers is half of the total
+    # If the total number of integers is odd, then there will be one more odd number than even numbers
+    # So, we calculate the number of odd numbers by doing integer division of total_numbers by 2 and adding the remainder
+    # This is because in a sequence of consecutive integers, the number of odd integers is equal to the number of even integers plus one if the total count is odd
+    odd_count = (total_numbers + 1) // 2
     
-    # So, we can use the formula: (total_numbers + 1) // 2 if low is odd, otherwise total_numbers // 2
-    # We can use the fact that low % 2 == 1 if low is odd, and low % 2 == 0 if low is even
-    # So, we can use the formula: (total_numbers + low % 2) // 2
+    # If low is even, then the first number in the range is even, so we need to subtract one from the odd count
+    # This is because we have counted one extra even number as an odd number
+    if low % 2 == 0:
+        odd_count -= 1
     
-    return (total_numbers + low % 2) // 2
+    # Return the count of odd numbers
+    return odd_count
