cd842f9d07df9bea5b0a9a0a302682c3f04db7ce
Tharuneswar <dtharuneswar@aditya.ac.in>
2026-07-31 09:30:02 +0000

    Solved Maximum Number of Balloons (approach 2)

--- maximum-number-of-balloons.py
+++ maximum-number-of-balloons.py
@@ -1,16 +1,18 @@
-def maxNumberOfBalloons(text):
-    # Create a dictionary to store the frequency of each character in 'balloon'
-    freq = {'b': 0, 'a': 0, 'l': 0, 'o': 0, 'n': 0}
+# Solution approach 2 - provide an efficient python solution with detailed inline comments explaining each step
+
+def maxNumberOfBalloons(text: str) -> int:
+    # Create a dictionary to store the frequency of each character in the string 'balloon'
+    balloon_freq = {'b': 0, 'a': 0, 'l': 0, 'o': 0, 'n': 0}
     
-    # Iterate over each character in the input text
+    # Iterate over each character in the string 'text'
     for char in text:
-        # If the character is in 'balloon', increment its frequency
-        if char in freq:
-            freq[char] += 1
+        # If the character is in the 'balloon_freq' dictionary, increment its frequency
+        if char in balloon_freq:
+            balloon_freq[char] += 1
     
-    # Since 'l' and 'o' appear twice in 'balloon', divide their frequencies by 2
-    freq['l'] //= 2
-    freq['o'] //= 2
+    # Since 'l' and 'o' appear twice in the string 'balloon', we need to divide their frequencies by 2
+    balloon_freq['l'] //= 2
+    balloon_freq['o'] //= 2
     
-    # The maximum number of 'balloon' that can be formed is the minimum frequency of its characters
-    return min(freq.values())
+    # The maximum number of 'balloon' strings that can be formed is the minimum frequency of all characters
+    return min(balloon_freq.values())
