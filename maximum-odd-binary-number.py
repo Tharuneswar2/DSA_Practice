2543a5e024b4a6db8b50597607b6e0e86520c762
Tharuneswar <dtharuneswar@aditya.ac.in>
2026-07-31 09:28:31 +0000

    Solved Maximum Odd Binary Number (approach 2)

--- maximum-odd-binary-number.py
+++ maximum-odd-binary-number.py
@@ -1,22 +1,22 @@
 # Solution approach 2 - provide an efficient python solution with detailed inline comments explaining each step
-
 def maximumOddBinaryNumber(n):
     # Convert the number to binary and remove the '0b' prefix that Python adds
     binary = bin(n)[2:]
     
     # Find the first occurrence of '0' from the right
-    # This is because we want to maximize the number of '1's on the left
-    zero_index = binary.rfind('0')
+    # This is because we want to replace the rightmost '0' with '1' to make it odd
+    rightmost_zero = binary.rfind('0')
     
-    # If no '0' is found, it means the number is already a maximum odd binary number
-    if zero_index == -1:
-        # So, we simply return the number as it is
+    # If no '0' is found, it means the number is already odd
+    # In this case, we simply return the number as it is
+    if rightmost_zero == -1:
         return n
     
-    # If '0' is found, we replace it with '1' to maximize the number of '1's on the left
-    # We also make sure that the number remains odd by appending '1' at the end
-    # We use string slicing to replace the '0' with '1' and append '1' at the end
-    binary = binary[:zero_index] + '1' + binary[zero_index+1:] + '1'
+    # Replace the rightmost '0' with '1'
+    # This will make the number odd
+    binary = binary[:rightmost_zero] + '1' + binary[rightmost_zero + 1:]
     
-    # Finally, we convert the binary string back to an integer and return it
-    return int(binary, 2)
+    # Convert the binary string back to an integer
+    result = int(binary, 2)
+    
+    return result
