8d0fa6d1788551d912d8b4236046a7d75078fe41
Tharuneswar <dtharuneswar@aditya.ac.in>
2026-08-01 08:42:58 +0000

    Solved Valid Word (approach 2)

--- valid-word.py
+++ valid-word.py
@@ -1,43 +1,28 @@
-def valid_word_abbreviation(word, abbr):
-    # Initialize two pointers, one for the word and one for the abbreviation
-    word_pointer = 0
-    abbr_pointer = 0
-    
-    # Loop through the abbreviation
-    while abbr_pointer < len(abbr):
-        # If the current character in the abbreviation is a digit
-        if abbr[abbr_pointer].isdigit():
-            # Initialize a variable to store the number
-            num = 0
-            
-            # Loop through the abbreviation until a non-digit character is found
-            while abbr_pointer < len(abbr) and abbr[abbr_pointer].isdigit():
-                # Multiply the current number by 10 and add the new digit
-                num = num * 10 + int(abbr[abbr_pointer])
-                abbr_pointer += 1
-            
-            # Move the word pointer by the number
-            word_pointer += num
-            
-            # If the word pointer is beyond the length of the word, return False
-            if word_pointer > len(word):
-                return False
-        
-        # If the word pointer is equal to the length of the word, return False
-        elif word_pointer == len(word):
-            return False
-        
-        # If the current character in the word does not match the current character in the abbreviation, return False
-        elif word[word_pointer] != abbr[abbr_pointer]:
-            return False
-        
-        # Move both pointers
-        word_pointer += 1
-        abbr_pointer += 1
-    
-    # If the word pointer is not equal to the length of the word, return False
-    if word_pointer != len(word):
+# Solution approach 2 - provide an efficient python solution with detailed inline comments explaining each step
+
+def validWordSquare(words):
+    # Check if the input is a list of strings
+    if not isinstance(words, list) or not all(isinstance(word, str) for word in words):
         return False
+
+    # Get the number of words
+    n = len(words)
     
+    # Check if the number of words is equal to the length of the first word
+    if n != len(words[0]):
+        return False
+
+    # Iterate over each word
+    for i in range(n):
+        # Check if the length of the current word is equal to the number of words
+        if len(words[i]) != n:
+            return False
+        
+        # Iterate over each character in the current word
+        for j in range(n):
+            # Check if the character at the current position is equal to the character at the corresponding position in the other words
+            if i < n and j < len(words[j]) and words[i][j] != words[j][i]:
+                return False
+
     # If all checks pass, return True
     return True
