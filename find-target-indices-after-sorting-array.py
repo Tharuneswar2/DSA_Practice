2feca08e1d8888c2974344102da11a6b9e4306ad
Tharuneswar <dtharuneswar@aditya.ac.in>
2026-08-01 08:42:00 +0000

    Solved Find Target Indices After Sorting Array (approach 2)

--- find-target-indices-after-sorting-array.py
+++ find-target-indices-after-sorting-array.py
@@ -1,3 +1,4 @@
+# Solution approach 2 - provide an efficient python solution with detailed inline comments explaining each step
 def targetIndices(nums, target):
     # Count the number of elements less than the target
     less_than_target = sum(1 for num in nums if num < target)
@@ -5,6 +6,6 @@ def targetIndices(nums, target):
     # Count the number of elements equal to the target
     equal_to_target = nums.count(target)
     
-    # The target indices will be from the count of elements less than the target
+    # The target indices will be a range from the count of elements less than the target
     # to the count of elements less than the target plus the count of elements equal to the target
     return list(range(less_than_target, less_than_target + equal_to_target))
