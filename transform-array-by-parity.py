edb9a2e48af1a7ce31321a1b3c3a328cddd3fa50
Tharuneswar <dtharuneswar@aditya.ac.in>
2026-08-01 08:41:47 +0000

    Solved Transform Array by Parity (approach 2)

--- transform-array-by-parity.py
+++ transform-array-by-parity.py
@@ -1,28 +1,21 @@
-def sort_array_by_parity(nums):
+# Solution approach 2 - provide an efficient python solution with detailed inline comments explaining each step
+def sortArrayByParity(A):
     # Initialize two pointers, one at the start and one at the end of the array
-    left, right = 0, len(nums) - 1
+    left, right = 0, len(A) - 1
     
-    # Continue the process until the two pointers meet
+    # Continue the loop until the two pointers meet
     while left < right:
         # If the left element is even, move to the next element
-        if nums[left] % 2 == 0:
+        if A[left] % 2 == 0:
             left += 1
         # If the right element is odd, move to the previous element
-        elif nums[right] % 2 != 0:
+        elif A[right] % 2 != 0:
             right -= 1
         # If the left element is odd and the right element is even, swap them
         else:
-            nums[left], nums[right] = nums[right], nums[left]
+            A[left], A[right] = A[right], A[left]
             left += 1
             right -= 1
-    
-    return nums
-
-# Alternatively, you can use a more Pythonic way to solve this problem
-def sort_array_by_parity_pythonic(nums):
-    # Separate the even and odd numbers into two lists
-    even = [num for num in nums if num % 2 == 0]
-    odd = [num for num in nums if num % 2 != 0]
-    
-    # Combine the two lists to get the final result
-    return even + odd
+            
+    # Return the modified array
+    return A
