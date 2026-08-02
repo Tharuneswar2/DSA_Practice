177152be57df3a8ea9ceeca6b0fe197db4c7fde6
Tharuneswar <dtharuneswar@aditya.ac.in>
2026-08-02 04:07:03 +0000

    Solved Reformat Phone Number (approach 2)

--- reformat-phone-number.py
+++ reformat-phone-number.py
@@ -1,24 +1,29 @@
-def reformatNumber(number: str) -> str:
-    # Remove all non-digit characters from the string
+# Solution approach 2 - provide an efficient python solution with detailed inline comments explaining each step
+
+def reformatNumber(number):
+    # Remove all non-digit characters from the phone number
     digits = ''.join(filter(str.isdigit, number))
     
-    # Initialize an empty list to store the reformatted groups of digits
-    groups = []
+    # Initialize an empty list to store the reformatted phone number
+    reformatted_number = []
     
-    # While there are still digits left to process
-    while len(digits) > 0:
-        # If there are 4 or more digits left, add a group of 3 digits
-        if len(digits) >= 4:
-            groups.append(digits[:3])
+    # Loop through the digits until there are 4 or less digits left
+    while len(digits) > 4:
+        # If there are more than 4 digits left, append the first 3 digits to the reformatted number
+        if len(digits) > 4:
+            reformatted_number.append(digits[:3])
+            # Remove the first 3 digits from the remaining digits
             digits = digits[3:]
-        # If there are 2 digits left, add them as a group
-        elif len(digits) == 2:
-            groups.append(digits)
-            digits = ''
-        # If there is only 1 digit left, add it to the previous group
-        elif len(digits) == 1:
-            groups[-1] += digits
-            digits = ''
+        # If there are exactly 4 digits left, append the first 2 digits and the last 2 digits separately to the reformatted number
+        else:
+            reformatted_number.append(digits[:2])
+            reformatted_number.append(digits[2:])
+            # Break the loop since there are no more digits left
+            break
     
-    # Join the groups with '-' and return the result
-    return '-'.join(groups)
+    # If there are 2 or less digits left, append them to the reformatted number
+    if digits:
+        reformatted_number.append(digits)
+    
+    # Join the reformatted number with '-' and return the result
+    return '-'.join(reformatted_number)
