6cc54915b7dd7d4b83ce328e6b938af5d6d3ed0a
Tharuneswar <dtharuneswar@aditya.ac.in>
2026-08-03 10:24:50 +0000

    Solved Earliest Finish Time for Land and Water Rides I (approach 2)

--- earliest-finish-time-for-land-and-water-rides-i.py
+++ earliest-finish-time-for-land-and-water-rides-i.py
@@ -1,22 +1,18 @@
 # Solution approach 2 - provide an efficient python solution with detailed inline comments explaining each step
-
 def earliestFinishTime(rides):
     # Sort the rides based on their finish times
     rides.sort(key=lambda x: x[1])  # x[1] represents the finish time of each ride
     
-    # Initialize the earliest finish time as the finish time of the first ride
-    earliest_finish_time = rides[0][1]
+    # Initialize the earliest finish time and the number of rides taken
+    earliest_finish_time = 0
+    num_rides_taken = 0
     
-    # Iterate through the rides starting from the second ride
-    for i in range(1, len(rides)):
-        # If the start time of the current ride is greater than or equal to the earliest finish time,
-        # update the earliest finish time as the finish time of the current ride
-        if rides[i][0] >= earliest_finish_time:
-            earliest_finish_time = rides[i][1]
-        # If the start time of the current ride is less than the earliest finish time,
-        # update the earliest finish time as the maximum of the current earliest finish time and the finish time of the current ride
-        else:
-            earliest_finish_time = max(earliest_finish_time, rides[i][1])
+    # Iterate over each ride
+    for start, finish in rides:
+        # If the current ride starts after the earliest finish time, update the earliest finish time
+        if start >= earliest_finish_time:
+            earliest_finish_time = finish  # Update the earliest finish time
+            num_rides_taken += 1  # Increment the number of rides taken
     
-    # Return the earliest finish time
-    return earliest_finish_time
+    # Return the number of rides taken
+    return num_rides_taken
