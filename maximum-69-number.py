b456be7aaf3688b242ec7097ce10a1af09d65c02
Tharuneswar <dtharuneswar@aditya.ac.in>
2026-08-04 09:24:30 +0000

    Solved Maximum 69 Number (approach 2)

--- maximum-69-number.py
+++ maximum-69-number.py
@@ -1,14 +1,15 @@
-def maximum69Number(num):
-    # Convert the number to a list of characters for easier manipulation
+# Solution approach 2 - provide an efficient python solution with detailed inline comments explaining each step
+def maximum69Number (num):
+    # Convert the number to a list of characters to easily manipulate digits
     num_list = list(str(num))
     
-    # Iterate over the list to find the first occurrence of '6'
+    # Iterate over each digit in the number
     for i in range(len(num_list)):
-        # If '6' is found, replace it with '9' to maximize the number
+        # If the current digit is 6, replace it with 9 to maximize the number
         if num_list[i] == '6':
             num_list[i] = '9'
-            # Break the loop as we only need to replace the first '6'
+            # Break the loop as we only need to replace the first 6 with 9
             break
     
-    # Join the list back into a string and convert it to an integer
+    # Join the list of characters back into a string and convert it to an integer
     return int(''.join(num_list))
