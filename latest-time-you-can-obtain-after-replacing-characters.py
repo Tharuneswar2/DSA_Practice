a41d458fe1f124a252d2dfbde5d1391f7e9c2484
Tharuneswar <dtharuneswar@aditya.ac.in>
2026-08-06 09:33:49 +0000

    Solved Latest Time You Can Obtain After Replacing Characters (approach 2)

--- latest-time-you-can-obtain-after-replacing-characters.py
+++ latest-time-you-can-obtain-after-replacing-characters.py
@@ -1,20 +1,24 @@
+# Solution approach 2 - provide an efficient python solution with detailed inline comments explaining each step
 def maximumTime(time: str) -> str:
     # Convert the input string into a list of characters for easier manipulation
     time_list = list(time)
     
-    # If the first character is '?', replace it with '2' if the second character is '?' or less than '4', otherwise replace it with '1'
+    # Check if the first character is '?' and if so, replace it with '2' (the maximum possible hour)
     if time_list[0] == '?':
-        time_list[0] = '2' if time_list[1] in ['?', '0', '1', '2', '3'] else '1'
+        time_list[0] = '2'
     
-    # If the second character is '?', replace it with '9' if the first character is '2', otherwise replace it with '3' if the first character is '1', otherwise replace it with '9'
+    # Check if the second character is '?' and if so, replace it with '3' if the first character is '2', otherwise replace it with '9'
     if time_list[1] == '?':
-        time_list[1] = '9' if time_list[0] == '2' else '3' if time_list[0] == '1' else '9'
+        if time_list[0] == '2':
+            time_list[1] = '3'
+        else:
+            time_list[1] = '9'
     
-    # If the third character is '?', replace it with '5'
+    # Check if the third character is '?' and if so, replace it with '5' (the maximum possible minute)
     if time_list[3] == '?':
         time_list[3] = '5'
     
-    # If the fourth character is '?', replace it with '9'
+    # Check if the fourth character is '?' and if so, replace it with '9' (the maximum possible minute)
     if time_list[4] == '?':
         time_list[4] = '9'
     
