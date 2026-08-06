69d4b8d56f67829d54ea1c2453aa9f093ac259b8
Tharuneswar <dtharuneswar@aditya.ac.in>
2026-08-06 03:49:29 +0000

    Solved Truncate Sentence (approach 2)

--- truncate-sentence.py
+++ truncate-sentence.py
@@ -1,14 +1,13 @@
-def truncate_sentence(sentence, k):
-    # Split the sentence into words
-    words = sentence.split()
+# Solution approach 2 - provide an efficient python solution with detailed inline comments explaining each step
+def truncateSentence(s, k):
+    # Split the input string into a list of words
+    words = s.split()
     
-    # If the number of words is less than or equal to k, return the original sentence
-    if len(words) <= k:
-        return sentence
+    # Use list slicing to get the first k words
+    truncated_words = words[:k]
     
-    # Otherwise, truncate the sentence to k words and join them back into a string
-    else:
-        return ' '.join(words[:k]) + '...'
-
-# Test the function
-print(truncate_sentence("Hello world this is a test", 3))
+    # Join the truncated words back into a string separated by spaces
+    truncated_sentence = ' '.join(truncated_words)
+    
+    # Return the truncated sentence
+    return truncated_sentence
