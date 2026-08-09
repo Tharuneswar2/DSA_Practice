615fc56f744e7c3c55c100cb33e498c13778edd7
Tharuneswar <dtharuneswar@aditya.ac.in>
2026-08-09 02:37:58 +0000

    Solved Longest Nice Substring (approach 2)

--- longest-nice-substring.py
+++ longest-nice-substring.py
@@ -1,25 +1,22 @@
-def longestNiceSubstring(s: str) -> str:
+# Solution approach 2 - provide an efficient python solution with detailed inline comments explaining each step
+def longestNiceSubstring(s):
     def is_nice(substring):
-        # Check if a substring is nice
-        for char in substring:
-            if char.isupper():
-                if char.lower() not in substring:
-                    return False
-            else:
-                if char.upper() not in substring:
-                    return False
-        return True
+        # Check if the substring is nice by verifying that for each character, 
+        # its lowercase and uppercase versions are both present in the substring
+        return all(lower in substring and upper in substring 
+                   for lower, upper in zip('abcdefghijklmnopqrstuvwxyz', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
 
-    def backtrack(start, path):
-        # Backtrack to find the longest nice substring
+    def dfs(start, path):
+        # Perform a depth-first search to find the longest nice substring
         if is_nice(path):
-            nonlocal res
-            if len(path) > len(res):
-                res = path
-        for end in range(start + 1, len(s) + 1):
-            backtrack(end, path + s[end - 1])
+            # If the current path is nice, return it
+            return path
+        for i in range(start, len(s)):
+            # Recursively explore all possible substrings
+            result = dfs(i + 1, path + s[i])
+            if result:
+                # If a nice substring is found, return it
+                return result
+        return ''
 
-    res = ""
-    for i in range(len(s)):
-        backtrack(i + 1, s[i])
-    return res
+    return dfs(0, '')
